70cdc9d7f08878abf715d113c9c69a73a59b9f43
Jason Kuhrt <jasonkuhrt@me.com>
2025-12-17 12:58:25 -0500

    simplify

--- repro.ts
+++ repro.ts
@@ -8,19 +8,21 @@
  * Trigger: declaration:true + class/namespace merge + recursive types
  */
 
-type U<$Obj, $Path extends string, $Value> = $Path extends `${infer Key}.${infer Rest}`
-  ? $Obj extends Record<any, any>
-    ? Key extends keyof $Obj ? Omit<$Obj, Key> & { [k in Key]: U<$Obj[k], Rest, $Value> }
-    : $Obj
-  : $Obj
-  : $Obj extends Record<any, any>
-    ? $Path extends keyof $Obj ? Omit<$Obj, $Path> & { [k in $Path]: $Value }
-    : $Obj
-  : $Obj
+type U<
+  $A,
+  $B extends string,
+  $C
+> =
+
+$B extends ``
+  ? any
+  : $A extends any
+    ? Omit<$A, ''> & { [_ in $B]: $C }
+  : any
 
 export class A {
   static fromString = <const $input extends string>(
-    input: A.Analyze<$input> extends { long: string } ? $input : never,
+    input: A.Thing<$input> extends { long: string } ? $input : never,
   ) => null as any
 }
 
@@ -28,14 +30,23 @@ export namespace A {
   export type Name = { aliases: string[]; long: string | null }
   export type NameEmpty = { aliases: []; long: null }
 
-  type Add<$N extends Name, $V extends string> =
-    $N['long'] extends null ? U<$N, 'long', $V> : U<$N, 'aliases', [...$N['aliases'], $V]>
+  type Add<
+    $N extends Name,
+    $V extends string
+  > =
+    $N['long'] extends null
+    ? U<$N, 'long', $V>
+    : U<$N, 'aliases', [...$N['aliases'], $V]>
 
-  export type Analyze<$E extends string, $N extends Name = NameEmpty> = _Analyze<$E, $N>
+  export type Thing<
+    $E extends string,
+    $N extends Name = NameEmpty
+  > =
+    _Thing<$E, $N>
 
-  type _Analyze<$E extends string, $N extends Name> =
+  type _Thing<$E extends string, $N extends Name> =
     $E extends `` ? $N :
-    $E extends `--${infer v} ${infer tail}` ? _Analyze<tail, Add<$N, v>> :
-    $E extends `--${infer v}` ? Add<$N, v> :
+    $E extends `${infer v} ${infer tail}` ? _Thing<tail, Add<$N, v>> :
+    $E extends `${infer v}` ? Add<$N, v> :
     'unknown'
 }
